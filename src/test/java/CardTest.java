a80721aa5d410de17bb8236a31c02cd32c98b795
Mikhail Gozhenko <sacred9900@gmail.com>
2021-11-09 23:11:32 +0500

    Added gitignore, delet folders

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -21,20 +21,21 @@ class CardDeliveryTest {
     }
 
     @Test
-    void shouldPositiveTest() {//позитивный тест с датой больше на 4 дня от фактической
-        LocalDate today = LocalDate.now().plusDays(4);
+    void shouldCalendarTest() {//позитивный тест с выбором в календаре и двумя буквами в поле город
+        LocalDate today = LocalDate.now().plusDays(7);
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("То");
+        $$("[class=menu-item__control").findBy(text("Томск")).click();
+        $("[data-test-id=date] input").click();
 
-        $("[data-test-id=city] input").setValue("Томск");
-        /*$$("[class=menu-item__control").findBy(text("Томск")).click();*/
-        $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
+
         $("[data-test-id=name] input").setValue("Агутин Леонид");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15)); // проверяем совпадение текста в уведомлении, в течении 15 секунд.
-        $(byText("Встреча успешно забронирована на")).shouldBe(visible);// проверяем совпадение текста в уведомлении,
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText("Встреча успешно забронирована на")).shouldBe(visible);
     }
 
     @Test
@@ -42,14 +43,31 @@ class CardDeliveryTest {
         LocalDate today = LocalDate.now();
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Ека"); // тут вводим первые три буквы (из второй задачи)
-        $$("[class=menu-item__control").findBy(text("Екатеринбург")).click(); // и выбираем из выпадающего списка элемент с полным совпадением.
+        $("[data-test-id=city] input").setValue("Ека");
+        $$("[class=menu-item__control").findBy(text("Екатеринбург")).click();
         $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
         $("[data-test-id=name] input").setValue("Николай Расторгуев");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(byText("Заказ на выбранную дату невозможен")).shouldBe(visible, Duration.ofSeconds(15)); // проверяем совпадение текста в уведомлении, в течении 15 секунд.
+        $(byText("Заказ на выбранную дату невозможен")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void shouldPositiveTest() {//позитивный тест с датой больше на 4 дня от фактической
+        LocalDate today = LocalDate.now().plusDays(4);
+        String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        open("http://localhost:9999");
 
+        $("[data-test-id=city] input").setValue("То");
+        $$("[class=menu-item__control").findBy(text("Томск")).click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
+
+        $("[data-test-id=name] input").setValue("Агутин Леонид");
+        $("[data-test-id=phone] input").setValue("+79617418596");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText("Встреча успешно забронирована на")).shouldBe(visible);
     }
 }
