c387d59914c156778fc6c9dc92ba39732aba368b
Mikhail Gozhenko <sacred9900@gmail.com>
2021-11-09 23:42:48 +0500

    Added metods. inputDate & actualDate

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -11,13 +11,25 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 
 class CardDeliveryTest {
-    @BeforeEach
-    private void openDriver (){Selenide.open("http://localhost:9999");};
 
+    public static String inputDate (int date) {
+        LocalDate today = LocalDate.now().plusDays(date);
+        return today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    public static String actualDate () {
+        LocalDate today = LocalDate.now();
+        return today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @BeforeEach
+    private void openDriver() {
+        Selenide.open("http://localhost:9999");
+    }
 
     @AfterEach
     public void closeDriver() {
@@ -41,24 +53,24 @@ class CardDeliveryTest {
 
     @Test
     void shouldIncorrectDateTest() { //негативный тест с фактической датой
-        LocalDate today = LocalDate.now();
-        String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
         $("[data-test-id=city] input").setValue("Ека");
         $$("[class=menu-item__control").findBy(text("Екатеринбург")).click();
-        $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
+        $("[data-test-id=date] input").doubleClick().sendKeys(actualDate());
         $("[data-test-id=name] input").setValue("Николай Расторгуев");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
         $(byText("Заказ на выбранную дату невозможен")).shouldBe(visible, Duration.ofSeconds(15));
     }
+
     @Test
     void shouldPositiveTest() {//позитивный тест с датой больше на 4 дня от фактической
         LocalDate today = LocalDate.now().plusDays(4);
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=city] input").setValue("То");
         $$("[class=menu-item__control").findBy(text("Томск")).click();
-        $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
+        $("[data-test-id=date] input").doubleClick().sendKeys( (4));
         $("[data-test-id=name] input").setValue("Агутин Леонид");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
