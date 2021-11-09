95a8174bad1e5f2f396c8236bbc83e686b9c499d
Mikhail Gozhenko <sacred9900@gmail.com>
2021-11-09 23:19:37 +0500

    added Url in @BeforeEach

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -14,6 +15,9 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 class CardDeliveryTest {
+    @BeforeEach
+    private void openDriver (){Selenide.open("http://localhost:9999");};
+
 
     @AfterEach
     public void closeDriver() {
@@ -24,12 +28,9 @@ class CardDeliveryTest {
     void shouldCalendarTest() {//позитивный тест с выбором в календаре и двумя буквами в поле город
         LocalDate today = LocalDate.now().plusDays(7);
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("То");
         $$("[class=menu-item__control").findBy(text("Томск")).click();
         $("[data-test-id=date] input").click();
-
-
         $("[data-test-id=name] input").setValue("Агутин Леонид");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
@@ -42,7 +43,6 @@ class CardDeliveryTest {
     void shouldIncorrectDateTest() { //негативный тест с фактической датой
         LocalDate today = LocalDate.now();
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Ека");
         $$("[class=menu-item__control").findBy(text("Екатеринбург")).click();
         $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
@@ -56,13 +56,9 @@ class CardDeliveryTest {
     void shouldPositiveTest() {//позитивный тест с датой больше на 4 дня от фактической
         LocalDate today = LocalDate.now().plusDays(4);
         String formedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        open("http://localhost:9999");
-
         $("[data-test-id=city] input").setValue("То");
         $$("[class=menu-item__control").findBy(text("Томск")).click();
-
         $("[data-test-id=date] input").doubleClick().sendKeys(formedDate);
-
         $("[data-test-id=name] input").setValue("Агутин Леонид");
         $("[data-test-id=phone] input").setValue("+79617418596");
         $("[data-test-id=agreement]").click();
